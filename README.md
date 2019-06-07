08395a4dc1d53ff7774b43f00f904fa93d0217b0
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-07 13:22:23 -1000

    initial commit of express server test

---
+++ README.md
@@ -0,0 +1,3 @@
+## Express.js Practice Server
+
+This project is to practice building a dummy server using express.  In it features a small store of data in order to practice implementing basic get requests via the server.
---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+const apps = require('./playstore');
+
+app.get('/apps', (req, res) => {
+  const { sort, genres } = req.query;
+
+  let results = apps;
+
+  if (sort) {
+    let sortToLower = sort.toLowerCase();
+    if(!['rating', 'app'].includes(sortToLower)) {
+      res.status(400).send('Sort must be either by rating or app');
+    }
+    results = apps.sort((a, b) => {
+      if(a[sort] < b[sort]) {return -1;}
+      if(a[sort] > b[sort]) {return 1;}
+      return 0;
+    });
+  }
+  if (genres) {
+    let genresToLower = genres.toLowerCase();
+    if(!['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(genresToLower)) {
+      res.status(400).send('Genre must be one of Action, Puzzle, Strategy, Casual, Arcade, or Card.');
+    }
+    results = results.filter(app => app.Genres === genres);
+  }
+  res
+    .json(results);
+});
+
+app.listen(8000, () => {
+  console.log('Server is listening on PORT:8000');
+});
